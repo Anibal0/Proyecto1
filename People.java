bbba94e78631803117708b6587b0bdba55976990
Anibal0 <43456623+Anibal0@users.noreply.github.com>
2018-11-29 19:38:03 -0600

    Create People.java

---
+++ People.java
@@ -0,0 +1,55 @@
+package com.example.anipa.myapplication;
+
+public class People {
+
+    private String name;
+    private String genero;
+    private String birthday;
+
+
+
+    public People(String name, String genero, String birthday )  {
+
+        this.name = name;
+        this.genero=genero;
+        this.birthday=birthday;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getGender() {
+        return genero;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+}
+
+    public Hero(String name, String genero, String birthday )  {
+
+        this.name = name;
+        this.genero=genero;
+        this.birthday=birthday;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+}
